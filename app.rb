835fff5900ea13b0d7ab58d4e869dbee8c77b782
Giedrius Rimkus <info@giedriusr.lt>
2017-08-24 20:47:06 +0300

    Adjust require path names for classes

--- app.rb
+++ app.rb
@@ -1,9 +1,9 @@
-require File.expand_path('/app/agent.rb', __FILE__)
-require File.expand_path('/app/caller.rb', __FILE__)
-require File.expand_path('/app/call_center.rb', __FILE__)
+require File.expand_path('../app/agent.rb', __FILE__)
+require File.expand_path('../app/caller.rb', __FILE__)
+require File.expand_path('../app/call_center.rb', __FILE__)
 
 call_center = CallCenter.new
 call_center.run
 
 puts "Average waiting time: #{call_center.avg_waiting_time}\n"
-puts "Average serving time: #{avg_serving_time}"
+puts "Average serving time: #{call_center.avg_serving_time}"
