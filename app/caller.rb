6e4dfada97887e4296e81ef28d3cefd3cf2a8087
Giedrius Rimkus <info@giedriusr.lt>
2017-08-24 20:53:20 +0300

    Add new properties for the caller

--- app/caller.rb
+++ app/caller.rb
@@ -1,4 +1,13 @@
 require File.expand_path('../person.rb', __FILE__)
 
 class Caller < Person
+  attr_reader :call_length, :call_at, :waited_for
+
+  def initialize(name, call_length = 2, call_at_minute = 0)
+    super(name)
+
+    @call_length = call_length
+    @call_at = call_at_minute
+    @waited_for = 0
+  end
 end
--- spec/app/caller_spec.rb
+++ spec/app/caller_spec.rb
@@ -1,10 +1,19 @@
 require File.expand_path('../../../app/caller.rb', __FILE__)
 
 RSpec.describe Caller do
-  describe 'object creation' do
-    it 'creates new caller with given name' do
-      caller = Caller.new('Dylan')
+  describe 'object values on initialization' do
+    let(:caller) { Caller.new('Dylan', 5, 12) }
+
+    it 'sets proper name' do
       expect(caller.name).to eq('Dylan')
+    end
+
+    it 'sets proper call_length' do
+      expect(caller.call_length).to eq(5)
+    end
+
+    it 'sets proper call_at' do
+      expect(caller.call_at).to eq(12)
     end
   end
 end
