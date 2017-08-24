815543d85ea060d949a3791062d5375a201ec2e6
Giedrius Rimkus <info@giedriusr.lt>
2017-08-24 20:58:36 +0300

    Seed call center with callers and agents

--- app.rb
+++ app.rb
@@ -1,8 +1,20 @@
+require 'faker'
+
 require File.expand_path('../app/agent.rb', __FILE__)
 require File.expand_path('../app/caller.rb', __FILE__)
 require File.expand_path('../app/call_center.rb', __FILE__)
 
-call_center = CallCenter.new
+callers = []
+50.times do |number|
+  callers << Caller.new("#{Faker::Name.name} (#{number})", Random.new.rand(2..20), number)
+end
+
+agents = []
+3.times do |number|
+  agents << Agent.new("#{Faker::Name.name} (#{number})")
+end
+
+call_center = CallCenter.new(callers, agents, 120)
 call_center.run
 
 puts "Average waiting time: #{call_center.avg_waiting_time}\n"
